01985fea91b6ad8f3a70820a4a4414eb21a0bd68
Gonzalo Gomez <gonzalo.gomez11@gmail.com>
2018-10-17 16:43:57 -0600

    Program now handles POST requests to /hash

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 		"fmt"
 		"encoding/base64"
 		"crypto/sha512"
+		"net/http"
+		"encoding/json"
 		)
 
 func EncodePassword(password string) string {
@@ -13,11 +15,25 @@ func EncodePassword(password string) string {
 	return sha512_hash
 }
 
+func hash(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    	case "POST":
+    		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+    		if err := r.ParseForm(); err != nil {
+    			fmt.Fprintf(w, "ParseForm() err: %v", err)
+    			return
+    		}
+    		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+    		password := r.FormValue("password")
+    		password = EncodePassword(password)
+    		json.NewEncoder(w).Encode(password)
+    	default:
+    		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+    }
+}
+
 func main() {
-	var password string
-	for password != "q" {
-		fmt.Printf("Enter text or q to quit: ")
-		fmt.Scanln(&password)
-		fmt.Printf(EncodePassword(password))
-	}
+	router := http.NewServeMux()
+	router.HandleFunc("/hash", hash)
+	http.ListenAndServe(":8080", router)	
 }
