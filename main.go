bccfca19dd292487cba649fac833b99fca6fd997
Gonzalo Gomez <gonzalo.gomez11@gmail.com>
2018-10-17 22:08:20 -0600

    Added catch all handler, return approriate responses when password is not set and when unknow url is used

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-		"fmt"
 		"encoding/base64"
 		"crypto/sha512"
 		"net/http"
 		"encoding/json"
+		"time"
+		"log"
 		)
 
 func EncodePassword(password string) string {
@@ -20,20 +21,37 @@ func hash(w http.ResponseWriter, r *http.Request) {
     	case "POST":
     		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
     		if err := r.ParseForm(); err != nil {
-    			fmt.Fprintf(w, "ParseForm() err: %v", err)
+    			log.Fatal(w, "ParseForm() err: %v", err)
     			return
     		}
-    		fmt.Fprintf(w, "Post from website! r.PostFrom = %v\n", r.PostForm)
+
+    		// Parse for password field
     		password := r.FormValue("password")
+    		if len(password) == 0 {
+    			json.NewEncoder(w).Encode("Password field was not set.")
+    			time.Sleep(5 * time.Second)
+    			return
+    		}
+
+    		// Encode passwword
     		password = EncodePassword(password)
+    		log.Printf("%s is encoding...\n", password)
+    		// Keep socket open for 5 seconds before sendng response
+    		time.Sleep(5 * time.Second)
     		json.NewEncoder(w).Encode(password)
     	default:
-    		fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+    		json.NewEncoder(w).Encode("Only POST method is supported for hash.")
     }
+}
+
+func catch_all(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	json.NewEncoder(w).Encode("Url is not supported.")
 }
 
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/hash", hash)
+	router.HandleFunc("/", catch_all)
 	http.ListenAndServe(":8080", router)	
 }
