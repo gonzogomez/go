eafdca0f072af95d28bdb10582de8bb755e3f0ce
Gonzalo Gomez <gonzalo.gomez11@gmail.com>
2018-10-18 10:37:02 -0600

    Added header with application type and status code for json repsonses

--- handlers.go
+++ handlers.go
@@ -19,23 +19,33 @@ func hash(w http.ResponseWriter, r *http.Request) {
     		// Parse for password field
     		password := r.FormValue("password")
     		if len(password) == 0 {
-    			json.NewEncoder(w).Encode("Password field was not set.")
-    			time.Sleep(5 * time.Second)
+    			sendError(w, http.StatusBadRequest, "Password field was not set")
     			return
     		}
 
     		// Encode passwword
     		password = EncodePassword(password)
     		log.Printf("Encoding Password...\n")
-    		// Keep socket open for 5 seconds before sendng response
-    		time.Sleep(5 * time.Second)
-    		json.NewEncoder(w).Encode(password)
+    		sendJson(w, http.StatusOK, map[string]string{"password": password})
     	default:
-    		json.NewEncoder(w).Encode("Only POST method is supported for hash.")
+    		sendError(w, http.StatusBadRequest, "Only POST method is supported for hash")		
     }
 }
 
 func catchAll(w http.ResponseWriter, r *http.Request) {
+	sendError(w, http.StatusBadRequest, "URL is not supported")
+}
+
+func sendError(w http.ResponseWriter, code int, message string) {
+	sendJson(w, code, map[string]string{"error": message})
+}
+
+func sendJson(w http.ResponseWriter, code int, payload interface{}) {
+	// Keep socket open for 5 seconds before sendng response
 	time.Sleep(5 * time.Second)
-	json.NewEncoder(w).Encode("Url is not supported.")
+	response, _ := json.Marshal(payload)
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(response)
 }
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	// create router class and lunch server
 	router := NewRouter()
 	http.ListenAndServe(":8080", router)	
 }
